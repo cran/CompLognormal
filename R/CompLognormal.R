0b57eca246cdbc417fe404b5672efefa030719cf
Saralees Nadarajah <Saralees.Nadarajah@manchester.ac.uk>
2012-10-04 00:00:00 +0000

    version 2.0

--- R/CompLognormal.R
+++ R/CompLognormal.R
@@ -1,93 +1,72 @@
 
 
-dcomplnorm<-function(x, spec, sigma=1, theta=1, ...)
-{	
-	add.pars	<- match.call(expand.dots=FALSE)$...
-	add.pars.val<- as.numeric(paste(match.call(expand.dots=FALSE)$...))
 
-	fun1.args	<- function(y) list(y, meanlog=mu, sdlog=sigma)
-	fun1		<- function(d) {function(z) do.call(paste(d,"lnorm",sep=""), fun1.args(z))}
-	fun2.args	<- function(y)
-	{
-		fun.args						<- list()
-		fun.args						<- setNames(fun.args[1:(length(add.pars)+1)],c("", names(add.pars)))
-		fun.args[[1]]					<- y
-		fun.args[2:(length(add.pars)+1)]		<- add.pars.val
-		return(fun.args)
-	}
-	fun2		<- function(d) {function(z) do.call(paste(d,spec,sep=""), fun2.args(z))}
+dcomplnorm<-function(x, spec, sigma=1, theta=1, log=FALSE, ...)
+{
+        f2=function (z, ...) {do.call(paste("d",spec,sep=""),list(z, ...))}
+        F2=function (z, ...) {do.call(paste("p",spec,sep=""),list(z, ...))}
+        ff=function (z) {f2(z, ...)}
+        f2p=grad(ff,theta)
 
-	mu		<- log(theta) + sigma^2 + theta*sigma^2*grad(fun2("d"),theta) / fun2("d")(theta)
-	phi		<- ( fun1("d")(theta)*(1-fun2("p")(theta) ) / ( fun2("d")(theta)*fun1("p")(theta) ) )
+	mu		<- log(theta) + sigma^2 + theta*sigma^2*f2p / f2(theta, ...)
+        f1=function (z) {dlnorm(z,meanlog=mu,sdlog=sigma)}
+        F1=function (z) {plnorm(z,meanlog=mu,sdlog=sigma)}
+        f1L=function (z) {dlnorm(z,meanlog=mu,sdlog=sigma,log=TRUE)}
+        F1L=function (z) {plnorm(z,meanlog=mu,sdlog=sigma,log.p=TRUE)}
+	phi		<- ( f1(theta)*(1-F2(theta, ...) ) / ( f2(theta, ...)*F1(theta) ) )
 
-	pdf		<- ifelse( x<0|x==0, 0,
-				ifelse( x<theta | x==theta,
-				(1/(1+phi)) / fun1("p")(theta)*fun1("d")(x),
-				(phi/(1+phi)) / (1-fun2("p")(theta))*fun2("d")(x)
-				)
-			   )
+	pdf=x
+        pdf[x<=theta&log==FALSE]=f1(x[x<=theta]) / ((1+phi)*F1(theta))
+        pdf[x<=theta&log==TRUE]=f1L(x[x<=theta])-log(1+phi)-F1L(theta)
+        pdf[x>theta&log==FALSE]=phi*f2(x[x>theta], ...)/((1+phi)*(1-F2(theta, ...)))
+        pdf[x>theta&log==TRUE]=log(phi)+log(f2(x[x>theta], ...))-log(1+phi)-log(1-F2(theta, ...))
 	return(pdf)
+
 }
 
 
-pcomplnorm<-function(x, spec, sigma=1, theta=1, ...)
-{	
 
-	add.pars	<- match.call(expand.dots=FALSE)$...
-	add.pars.val<- as.numeric(paste(match.call(expand.dots=FALSE)$...))
+pcomplnorm<-function(x, spec, sigma=1, theta=1, log.p=FALSE, ...)
+{
+        f2=function (z, ...) {do.call(paste("d",spec,sep=""),list(z, ...))}
+        F2=function (z, ...) {do.call(paste("p",spec,sep=""),list(z, ...))}
+        ff=function (z) {f2(z, ...)}
+        f2p=grad(ff,theta)
 
-	fun1.args	<- function(y) list(y, meanlog=mu, sdlog=sigma)
-	fun1		<- function(d) {function(z) do.call(paste(d,"lnorm",sep=""), fun1.args(z))}
-	fun2.args	<- function(y)
-	{
-		fun.args						<- list()
-		fun.args						<- setNames(fun.args[1:(length(add.pars)+1)],c("", names(add.pars)))
-		fun.args[[1]]					<- y
-		fun.args[2:(length(add.pars)+1)]		<- add.pars.val
-		return(fun.args)
-	}
-	fun2		<- function(d) {function(z) do.call(paste(d,spec,sep=""), fun2.args(z))}
+	mu		<- log(theta) + sigma^2 + theta*sigma^2*f2p / f2(theta, ...)
+        f1=function (z) {dlnorm(z,meanlog=mu,sdlog=sigma)}
+        F1=function (z) {plnorm(z,meanlog=mu,sdlog=sigma)}
+        F1L=function (z) {plnorm(z,meanlog=mu,sdlog=sigma,log.p=TRUE)}
+	phi		<- ( f1(theta)*(1-F2(theta, ...) ) / ( f2(theta, ...)*F1(theta) ) )
 
-	mu		<- log(theta) + sigma^2 + theta*sigma^2*grad(fun2("d"),theta) / fun2("d")(theta)
-	phi		<- ( fun1("d")(theta)*(1-fun2("p")(theta) ) / ( fun2("d")(theta)*fun1("p")(theta) ) )
-
-	cdf		<- ifelse( x<0 | x==0, 0,
-				ifelse( x<theta | x==theta,
-				(1/(1+phi))*fun1("p")(x) / fun1("p")(theta),
-				(1/(1+phi)) + (phi/(1+phi))*(fun2("p")(x)-fun2("p")(theta))/(1-fun2("p")(theta))
-				)
-			   )
+	cdf=x
+        cdf[x<=theta&log.p==FALSE]=F1(x[x<=theta]) / ((1+phi)*F1(theta))
+        cdf[x<=theta&log.p==TRUE]=F1L(x[x<=theta])-log(1+phi)-F1L(theta)
+        cdf[x>theta&log.p==FALSE]=(1/(1+phi)) + (phi/(1+phi))*(F2(x[x>theta], ...)-F2(theta, ...))/(1-F2(theta, ...))
+        cdf[x>theta&log.p==TRUE]=-log(1+phi)+log(1 + phi*(F2(x[x>theta], ...)-F2(theta, ...))/(1-F2(theta, ...)))
 	return(cdf)
 }
 
 
 
-qcomplnorm<-function(p, spec, sigma=1, theta=1, ...)
-{	
+qcomplnorm<-function(p, spec, sigma=1, theta=1, log.p=FALSE, ...)
+{
+        if (log.p==TRUE) p=exp(p)
+        f2=function (z, ...) {do.call(paste("d",spec,sep=""),list(z, ...))}
+        F2q=function (z, ...) {do.call(paste("q",spec,sep=""),list(z, ...))}
+        F2=function (z, ...) {do.call(paste("p",spec,sep=""),list(z, ...))}
+        ff=function (z) {f2(z, ...)}
+        f2p=grad(ff,theta)
 
-	add.pars	<- match.call(expand.dots=FALSE)$...
-	add.pars.val<- as.numeric(paste(match.call(expand.dots=FALSE)$...))
+	mu		<- log(theta) + sigma^2 + theta*sigma^2*f2p / f2(theta, ...)
+        f1=function (z) {dlnorm(z,meanlog=mu,sdlog=sigma)}
+        F1q=function (z) {qlnorm(z,meanlog=mu,sdlog=sigma)}
+        F1=function (z) {plnorm(z,meanlog=mu,sdlog=sigma)}
+	phi		<- ( f1(theta)*(1-F2(theta, ...) ) / ( f2(theta, ...)*F1(theta) ) )
 
-	fun1.args	<- function(y) list(y, meanlog=mu, sdlog=sigma)
-	fun1		<- function(d) {function(z) do.call(paste(d,"lnorm",sep=""), fun1.args(z))}
-	fun2.args	<- function(y)
-	{
-		fun.args						<- list()
-		fun.args						<- setNames(fun.args[1:(length(add.pars)+1)],c("", names(add.pars)))
-		fun.args[[1]]					<- y
-		fun.args[2:(length(add.pars)+1)]		<- add.pars.val
-		return(fun.args)
-	}
-	fun2		<- function(d) {function(z) do.call(paste(d,spec,sep=""), fun2.args(z))}
-
-	mu		<- log(theta) + sigma^2 + theta*sigma^2*grad(fun2("d"),theta) / fun2("d")(theta)
-	phi		<- ( fun1("d")(theta)*(1-fun2("p")(theta) ) / ( fun2("d")(theta)*fun1("p")(theta) ) )
-
-	qf		<- ifelse( p<0 | p>1, NaN,
-				c( fun1("q")( p[p<(1/(1+phi)) | p==(1/(1+phi))]*(1+phi) * fun1("p")(theta) ),
-				fun2("q")( (p[p>(1/(1+phi))]*(1+phi)-1)/phi * (1-fun2("p")(theta))
-                + fun2("p")(theta) ) )
-			   )
+	qf=p
+        qf[p<=1/(1+phi)]=F1q( p[p<=1/(1+phi)]*(1+phi) * F1(theta) )
+        qf[p>1/(1+phi)]=F2q( (p[p>(1/(1+phi))]*(1+phi)-1)/phi * (1-F2(theta, ...))+ F2(theta, ...), ...)
 	return(qf)
 }
 
@@ -95,67 +74,7 @@ qcomplnorm<-function(p, spec, sigma=1, theta=1, ...)
 
 rcomplnorm<-function(n, spec, sigma=1, theta=1, ...)
 {	
-	add.pars	<- match.call(expand.dots=FALSE)$...
-	add.pars.val<- as.numeric(paste(match.call(expand.dots=FALSE)$...))
-
-
-	fun1.args	<- function(y) list(y, meanlog=mu, sdlog=sigma)
-	fun1		<- function(d) {function(z) do.call(paste(d,"lnorm",sep=""), fun1.args(z))}
-	fun2.args	<- function(y)
-	{
-		fun.args						<- list()
-		fun.args						<- setNames(fun.args[1:(length(add.pars)+1)],c("", names(add.pars)))
-		fun.args[[1]]					<- y
-		fun.args[2:(length(add.pars)+1)]		<- add.pars.val
-		return(fun.args)
-	}
-	fun2		<- function(d) {function(z) do.call(paste(d,spec,sep=""), fun2.args(z))}
-
-	mu		<- log(theta) + sigma^2 + theta*sigma^2*grad(fun2("d"),theta) / fun2("d")(theta)
-	phi		<- ( fun1("d")(theta)*(1-fun2("p")(theta) ) / ( fun2("d")(theta)*fun1("p")(theta) ) )
-
-	rand		<- runif(n)
-
-	rf		<- ifelse( rand<0 | rand>1, 0,
-				c( fun1("q")( rand[rand<(1/(1+phi)) | rand==(1/(1+phi))] * (1+phi)
-                     * fun1("p")(theta) ),
-				fun2("q")( (rand[rand>(1/(1+phi))]*(1+phi)-1)/phi * (1-fun2("p")(theta))
-                    + fun2("p")(theta) ) )
-			   )
-	return(rf)
-}
-
-
-
-nlogl.complnorm<-function(p,x,spec,...)
-{	
-	sigma		<- p[1]
-	theta		<- p[2]
-	add.pars	<- paste(match.call(expand.dots=FALSE)$...)
-
-	tt						<- 1.0e20
-
-if(all(p>0)){
-
-	fun1.args	<- function(y) list(y, meanlog=mu, sdlog=sigma)
-	fun1		<- function(d) {function(z) do.call(paste(d,"lnorm",sep=""), fun1.args(z))}
-	fun2.args	<- function(y)
-	{
-		fun.args						<- list()
-		fun.args						<- setNames(fun.args[1:(length(add.pars)+1)],c("", add.pars))
-		fun.args[[1]]					<- y
-		fun.args[2:(length(add.pars)+1)]		<- p[3:(length(add.pars)+2)]
-		return(fun.args)
-	}
-	fun2		<- function(d) {function(z) do.call(paste(d,spec,sep=""), fun2.args(z))}
-
-	mu		<- log(theta) + sigma^2 + theta*sigma^2*grad(fun2("d"),theta) / fun2("d")(theta)
-	phi		<- fun1("d")(theta)*(1-fun2("p")(theta)) / ( fun2("d")(theta)*fun1("p")(theta) )
-
-	tt		<- -sum(log( (1/(1+phi))/fun1("p")(theta)*fun1("d")(x[x>0&x<theta])))
-        - sum(log( (phi/(1+phi))/(1-fun2("p")(theta))*fun2("d")(x[x>theta]) ))
-
-}
-
-return(tt)
+	u <- runif(n,min=0,max=1)
+        sf <- qcomplnorm(u, spec, sigma=sigma, theta=theta, ...)
+	return(sf)
 }
